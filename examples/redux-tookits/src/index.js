4512783fc42960bafc08acd15794eff31379eccc
nbili <coding520@163.com>
2022-02-27 17:05:24 +0800

    redux

--- examples/redux-tookits/src/index.js
+++ examples/redux-tookits/src/index.js
@@ -1,7 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./compose";
+import { combineReducers, createStore } from "redux";
+
+const initialState = {
+  users: [
+    { id: 1, name: "Kent" },
+    { id: 2, name: "Nbili" }
+  ],
+  tasks: [
+    {
+      title: "Leaning English"
+    },
+    {
+      title: "Coding programming"
+    },
+    {
+      title: "Interview"
+    }
+  ]
+};
+
+const ADD_USER = "ADD_USER";
+const ADD_TASK = "ADD_TASK";
+
+const addTask = title => ({ type: ADD_TASK, payload: title });
+const addUser = user => ({ type: ADD_USER, payload: user });
+
+const userReducer = (users = initialState.users, action) => {
+  if (action.type === ADD_USER) {
+    return [...users, action.payload];
+  }
+
+  return users;
+};
+
+const taskReducer = (tasks = initialState.tasks, action) => {
+  if (action.type === ADD_TASK) {
+    return [...tasks, action.payload];
+  }
+
+  return tasks;
+};
+
+const reducer = combineReducers({ users: userReducer, tasks: taskReducer });
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addTask({ title: `new Title` }));
+store.dispatch(addUser({ id: 3, name: `new Name` }));
 
 ReactDOM.render(
   <React.StrictMode>
