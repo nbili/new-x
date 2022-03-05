d13e65355313f56913c915fa64591df8a981794c
nbili <coding520@163.com>
2022-03-05 23:22:38 +0800

    style

--- examples/css-in-js/src/index.js
+++ examples/css-in-js/src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.4em;
+  text-align: "center";
+  color: blue;
+`;
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Title>css in js</Title>
+    </div>
+  );
 }
 
 ReactDOM.render(
