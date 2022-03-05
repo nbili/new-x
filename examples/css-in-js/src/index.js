cd4f54ccd1e9c4b7cd953544677372b065e80035
nbili <coding520@163.com>
2022-03-05 23:50:40 +0800

    style

--- examples/css-in-js/src/index.js
+++ examples/css-in-js/src/index.js
@@ -4,14 +4,20 @@ import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 1.4em;
-  text-align: "center";
+  text-align: center;
   color: blue;
+`;
+
+const Description = styled.p`
+  font-size: 1em;
+  color: gray;
 `;
 
 function App() {
   return (
     <div>
       <Title>css in js</Title>
+      <Description>this is a Description</Description>
     </div>
   );
 }
