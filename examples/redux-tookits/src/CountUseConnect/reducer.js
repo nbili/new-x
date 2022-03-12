b908ecc22319c252bcd14fee61bf7c451516a972
nbili <coding520@163.com>
2022-03-12 21:55:57 +0800

    immer

--- examples/redux-tookits/src/CountUseConnect/reducer.js
+++ examples/redux-tookits/src/CountUseConnect/reducer.js
@@ -1,10 +1,13 @@
 import { INCREMENT, DECREMENT, SET } from "./actions";
+import { produce } from "immer";
 
 export const initialState = { count: 38 };
 
 export const reducer = (state = initialState, action) => {
   if (action.type === INCREMENT) {
-    return { count: state.count + 1 };
+    return produce(state, draftState => {
+      draftState.count += 2;
+    });
   }
   if (action.type === DECREMENT) {
     return { count: state.count - 1 };
