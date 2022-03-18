2abbe9ee8bc10a0d059b7e3459bbd22758514cb9
nbili <coding520@163.com>
2022-03-18 22:47:30 +0800

    polaris

--- examples/shopify-polaris/src/App.js
+++ examples/shopify-polaris/src/App.js
@@ -1,5 +1,17 @@
+import { Page, Card, Button } from "@shopify/polaris";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Page title="Example app">
+        <Card sectioned>
+          <Button onClick={() => alert("Button clicked!")}>
+            Example button
+          </Button>
+        </Card>
+      </Page>
+    </div>
+  );
 }
 
 export default App;
--- examples/shopify-polaris/src/index.js
+++ examples/shopify-polaris/src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import enTranslations from "@shopify/polaris/locales/en.json";
+import { AppProvider } from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider i18n={enTranslations}>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
