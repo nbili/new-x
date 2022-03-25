4ceaa7ff5a535c442c362ffdc56ad07b945aec9e
nbili <coding520@163.com>
2022-03-25 23:51:24 +0800

    init

--- examples/css-in-js/src/components/ProgressBar/ProgressBar.js
+++ examples/css-in-js/src/components/ProgressBar/ProgressBar.js
@@ -1,5 +1,30 @@
-function ProgressBar() {
-  return `ProgressBar`;
+import styled from "styled-components";
+
+function ProgressBar({ value, size }) {
+  return (
+    <BarWrapper
+      role="progressbar"
+      aria-valuenow="20"
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      {/* 20 % */}
+      <Bar style={{ "--width": value + "%" }} />
+    </BarWrapper>
+  );
 }
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: 8px;
+  background-color: blue;
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
