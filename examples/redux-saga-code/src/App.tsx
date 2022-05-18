f537039448d93901d9a14a021132696d33e11b0c
nbili <coding520@163.com>
2022-05-18 23:43:27 +0800

    deps

--- examples/redux-saga-code/src/App.tsx
+++ examples/redux-saga-code/src/App.tsx
@@ -1,9 +1,7 @@
 import { Header } from "./components/Header";
 import "./App.css";
-import { generator } from "./saga";
 
 function App() {
-  generator();
   return (
     <div className="App">
       <Header />
